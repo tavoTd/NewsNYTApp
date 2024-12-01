cd094d677f9d8c44b4c336a3ae2c13d539a079b8
Gustavo Tellez Diaz <gustavo-td@hotmail.com>
2024-11-30 22:30:26 -0600

    Redesign news list

--- NewsNYTApp/Modules/ NewsList/NewsListTableViewCell.swift
+++ NewsNYTApp/Modules/ NewsList/NewsListTableViewCell.swift
@@ -24,9 +24,9 @@ class NewsListTableViewCell: UITableViewCell {
         return view
     }()
     
-    private lazy var imageNew: UIImageView = {
+    private lazy var imageNews: UIImageView = {
         let imageView = UIImageView()
-        imageView.contentMode = .scaleAspectFill
+        imageView.contentMode = .scaleAspectFit
         imageView.backgroundColor = UIColor.white
         imageView.roundCorners(corners: [.layerMinXMinYCorner, .layerMinXMaxYCorner],
                                radius: self.cornerRadius)
@@ -40,48 +40,22 @@ class NewsListTableViewCell: UITableViewCell {
         label.font = UIFont.boldSystemFont(ofSize: 16.0)
         label.textAlignment = NSTextAlignment.left
         label.textColor = UIColor.black
-        label.numberOfLines = 4
+        label.numberOfLines = 3
         label.adjustsFontSizeToFitWidth = true
-        label.minimumScaleFactor = 0.7
+        label.minimumScaleFactor = 0.6
         label.translatesAutoresizingMaskIntoConstraints = false
 
         return label
     }()
     
-    private lazy var stackView: UIStackView = {
-        let stack = UIStackView()
-        stack.axis = .horizontal
-        stack.spacing = 10.0
-        stack.distribution = .fillProportionally
-        stack.clipsToBounds = true
-        stack.roundCorners(corners: [.layerMaxXMaxYCorner],
-                           radius: self.cornerRadius)
-        stack.translatesAutoresizingMaskIntoConstraints = false
+    private lazy var imageNewsPaper: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "icon_newYorkTimes")
+        imageView.contentMode = .scaleAspectFill
+        imageView.backgroundColor = UIColor.white
+        imageView.translatesAutoresizingMaskIntoConstraints = false
 
-        return stack
-    }()
-    
-    private lazy var labelPublishedDate: UILabel = {
-        let label = UILabel()
-        label.font = UIFont.boldSystemFont(ofSize: 12.0)
-        label.textAlignment = NSTextAlignment.left
-        label.textColor = UIColor.black
-        label.numberOfLines = 1
-        label.translatesAutoresizingMaskIntoConstraints = false
-
-        return label
-    }()
-    
-    private lazy var labelSection: UILabel = {
-        let label = UILabel()
-        label.font = UIFont.boldSystemFont(ofSize: 12.0)
-        label.textAlignment = NSTextAlignment.center
-        label.textColor = UIColor.white
-        label.backgroundColor = UIColor.orange
-        label.numberOfLines = 1
-        label.translatesAutoresizingMaskIntoConstraints = false
-
-        return label
+        return imageView
     }()
     
     public static let identifier = "NewsListTableViewCell"
@@ -102,14 +76,10 @@ class NewsListTableViewCell: UITableViewCell {
         self.backgroundColor = UIColor.lightCyan
         self.contentView.backgroundColor = UIColor.lightCyan
         self.containerView.backgroundColor = UIColor.white
-        self.stackView.backgroundColor = UIColor.white
         
-        self.stackView.addArrangedSubview(labelPublishedDate)
-        self.stackView.addArrangedSubview(labelSection)
-        
-        self.containerView.addSubview(imageNew)
+        self.containerView.addSubview(imageNews)
         self.containerView.addSubview(labelTitleNew)
-        self.containerView.addSubview(stackView)
+        self.containerView.addSubview(imageNewsPaper)
         self.contentView.addSubview(containerView)
     }
     
@@ -117,31 +87,29 @@ class NewsListTableViewCell: UITableViewCell {
         NSLayoutConstraint.activate([
             containerView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10.0),
             containerView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20.0),
-            containerView.heightAnchor.constraint(equalToConstant: 120.0),
+            containerView.heightAnchor.constraint(equalToConstant: 100.0),
             containerView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20.0),
             containerView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10.0),
             
-            imageNew.topAnchor.constraint(equalTo: containerView.topAnchor),
-            imageNew.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
-            imageNew.widthAnchor.constraint(equalToConstant: 100.0),
-            imageNew.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
+            imageNews.topAnchor.constraint(equalTo: containerView.topAnchor),
+            imageNews.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+            imageNews.widthAnchor.constraint(equalToConstant: 150.0),
+            imageNews.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
             
-            labelTitleNew.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 20.0),
-            labelTitleNew.leadingAnchor.constraint(equalTo: imageNew.trailingAnchor, constant: 20.0),
-            labelTitleNew.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20.0),
-            labelTitleNew.bottomAnchor.constraint(equalTo: stackView.topAnchor, constant: -10.0),
-            
-            stackView.leadingAnchor.constraint(equalTo: imageNew.trailingAnchor, constant: 20.0),
-            stackView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
-            stackView.heightAnchor.constraint(equalToConstant: 30.0),
-            stackView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
+            labelTitleNew.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 10.0),
+            labelTitleNew.leadingAnchor.constraint(equalTo: imageNews.trailingAnchor, constant: 10.0),
+            labelTitleNew.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -10.0),
+
+            imageNewsPaper.topAnchor.constraint(equalTo: labelTitleNew.bottomAnchor),
+            imageNewsPaper.heightAnchor.constraint(equalToConstant: 10.0),
+            imageNewsPaper.widthAnchor.constraint(equalToConstant: 10.0),
+            imageNewsPaper.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -10.0),
+            imageNewsPaper.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -10.0)
         ])
     }
     
     func configure(with data: News) {
-        self.imageNew.image = data.image.normal
+        self.imageNews.image = data.image.normal
         self.labelTitleNew.text = data.title
-        self.labelSection.text = data.section
-        self.labelPublishedDate.text = data.publishedDate
     }
 }
